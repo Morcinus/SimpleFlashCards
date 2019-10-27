45f90c1ae3293d4ed62375783ae6ee845745262d
Morcinus <30431494+Morcinus@users.noreply.github.com>
2019-10-27 14:43:17 +0100

    Add Learn Buttons

--- simpleflashcards-client/src/pages/deck.js
+++ simpleflashcards-client/src/pages/deck.js
@@ -7,8 +7,13 @@ import Paper from "@material-ui/core/Paper";
 import Divider from "@material-ui/core/Divider";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import Typography from "@material-ui/core/Typography";
 import LibraryBooks from "@material-ui/icons/LibraryBooks";
 import Bookmarks from "@material-ui/icons/Bookmarks";
+import MenuBook from "@material-ui/icons/MenuBook";
+import FitnessCenter from "@material-ui/icons/FitnessCenter";
 
 import DeckInfo from "../components/DeckInfo";
 
@@ -98,7 +103,7 @@ export class deck extends Component {
                 {this.state.selectedTabIndex === 1 ? (
                   <div>ta1</div>
                 ) : (
-                  <div>ta2</div>
+                  learnButtons()
                 )}
 
                 <br />
@@ -109,6 +114,73 @@ export class deck extends Component {
       </div>
     );
   }
+}
+
+function learnButtons() {
+  return (
+    <Grid container direction="row" justify="space-evenly" alignItems="center">
+      <Card
+        variant="outlined"
+        style={{
+          width: "175px",
+          height: "175px",
+          marginRight: "20px",
+          backgroundColor: "#bff27e",
+          alignItems: "center"
+        }}
+      >
+        <CardActionArea
+          style={{ width: "100%", height: "100%", textAlign: "center" }}
+        >
+          <MenuBook style={{ fontSize: 75, color: "#37474f" }}></MenuBook>
+          <Typography style={{ fontWeight: "bold", color: "#37474f" }}>
+            LEARN NEW
+          </Typography>
+        </CardActionArea>
+      </Card>
+      <Card
+        variant="outlined"
+        style={{
+          width: "200px",
+          height: "200px",
+          marginRight: "20px",
+          backgroundColor: "#91cc47"
+        }}
+      >
+        <CardActionArea
+          style={{ width: "100%", height: "100%", textAlign: "center" }}
+        >
+          <MenuBook style={{ fontSize: 75, color: "#37474f" }}></MenuBook>
+          <FitnessCenter
+            style={{ fontSize: 75, color: "#37474f" }}
+          ></FitnessCenter>
+          <Typography style={{ fontWeight: "bold", color: "#37474f" }}>
+            LEARN & REVIEW
+          </Typography>
+        </CardActionArea>
+      </Card>
+      <Card
+        variant="outlined"
+        style={{
+          width: "175px",
+          height: "175px",
+          marginRight: "20px",
+          backgroundColor: "#bff27e"
+        }}
+      >
+        <CardActionArea
+          style={{ width: "100%", height: "100%", textAlign: "center" }}
+        >
+          <FitnessCenter
+            style={{ fontSize: 75, color: "#37474f" }}
+          ></FitnessCenter>
+          <Typography style={{ fontWeight: "bold", color: "#37474f" }}>
+            REVIEW OLD
+          </Typography>
+        </CardActionArea>
+      </Card>
+    </Grid>
+  );
 }
 
 deck.propTypes = {
