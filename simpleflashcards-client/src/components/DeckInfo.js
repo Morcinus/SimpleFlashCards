2e5cab95fbf3e57273c826d89541debca1c80723
Morcinus <30431494+Morcinus@users.noreply.github.com>
2019-10-30 15:50:46 +0100

    Add share button to DeckInfo

--- simpleflashcards-client/src/components/DeckInfo.js
+++ simpleflashcards-client/src/components/DeckInfo.js
@@ -4,16 +4,51 @@ import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
 import Typography from "@material-ui/core/Typography";
-import Tab from "@material-ui/core/Tab";
+import Popover from "@material-ui/core/Popover";
 import CardMedia from "@material-ui/core/CardMedia";
 import Card from "@material-ui/core/Card";
+import Input from "@material-ui/core/Input";
+import Box from "@material-ui/core/Box";
 import BookmarkBorder from "@material-ui/icons/BookmarkBorder";
 import Share from "@material-ui/icons/Share";
 
 export class DeckInfo extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      popoverOpen: false,
+      anchorEl: null,
+      copiedLink: false
+    };
   }
+
+  handleCopyClick = elementId => {
+    // Zdroj: https://stackoverflow.com/questions/39501289/in-reactjs-how-to-copy-text-to-clipboard
+    let textField = document.createElement("textarea");
+    textField.innerText = `${window.location.href}`;
+    let parentElement = document.getElementById(elementId);
+    parentElement.appendChild(textField);
+    textField.select();
+    document.execCommand("copy");
+    parentElement.removeChild(textField);
+
+    this.setState({
+      copiedLink: true
+    });
+  };
+
+  handlePopoverOpen = event => {
+    this.setState({
+      popoverOpen: true,
+      anchorEl: event.currentTarget
+    });
+  };
+
+  handlePopoverClose = () => {
+    this.setState({
+      popoverOpen: false
+    });
+  };
 
   render() {
     return (
@@ -66,9 +101,59 @@ export class DeckInfo extends Component {
             <BookmarkBorder /> <Typography> Pin</Typography>
           </Button>
 
-          <Button item variant="outlined" color="primary" size="large">
+          <Button
+            onClick={this.handlePopoverOpen}
+            item
+            variant="outlined"
+            color="primary"
+            size="large"
+          >
             <Share /> <Typography> Share</Typography>
           </Button>
+          <Popover
+            open={this.state.popoverOpen}
+            anchorEl={this.state.anchorEl}
+            onClose={this.handlePopoverClose}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center"
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center"
+            }}
+          >
+            <Box style={{ margin: "20px" }} id="popover">
+              <Typography style={{ fontWeight: "bold", color: "#37474f" }}>
+                Share this deck
+              </Typography>
+              <Input
+                defaultValue={`${window.location.href}`}
+                readOnly={true}
+                style={{ color: "#808080" }}
+              />
+              <Grid
+                container
+                direction="row"
+                justify="flex-start"
+                alignItems="center"
+              >
+                <Box item flexGrow={1}>
+                  <Typography style={{ color: "#37474f" }}>
+                    {this.state.copiedLink ? "Copied!" : ""}
+                  </Typography>
+                </Box>
+                <Button
+                  item
+                  color="secondary"
+                  style={{ fontWeight: "bold" }}
+                  onClick={() => this.handleCopyClick("popover")}
+                >
+                  Copy link
+                </Button>
+              </Grid>
+            </Box>
+          </Popover>
         </Grid>
       </Grid>
     );
