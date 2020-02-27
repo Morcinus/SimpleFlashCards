b410e36798441672fa8dfacaf6b8a6d3c77c00a7
Morcinus <30431494+Morcinus@users.noreply.github.com>
2020-02-27 14:58:07 +0100

    FB-Docs: Add authMiddleware, admin

--- firebase-functions/functions/util/admin.js
+++ firebase-functions/functions/util/admin.js
@@ -1,7 +1,18 @@
+/**
+ * @module admin
+ * @category util
+ * @requires {@link firebase-admin}
+ */
+
 const admin = require("firebase-admin");
 
+// Spuštění SDK
 admin.initializeApp();
 
+/**
+ * @const db
+ * @description Firestore Databáze
+ */
 const db = admin.firestore();
 
 module.exports = { admin, db };
--- firebase-functions/functions/util/authMiddleware.js
+++ firebase-functions/functions/util/authMiddleware.js
@@ -1,36 +1,44 @@
 const { admin, db } = require("./admin");
 
+/**
+ * @module authMiddleware
+ * @category util
+ * @description Slouží k ověřování uživatele pomocí idTokenu
+ * @param {Object} req - HTTP požadavek
+ * @param {Object} res - HTTP odpověď na požadavek
+ * @param {function} next - Callback funkce
+ */
+
 // Zdroj https://youtu.be/Fz1f7NLvcu4
-// NEEDS BETTER ERROR HANDLING
 module.exports = (req, res, next) => {
+  // Přiřazení ID tokenu
   let idToken;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer ")
-  ) {
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
     idToken = req.headers.authorization.split("Bearer ")[1];
   } else {
-    console.error("No token found");
     return res.status(403).json({ error: "Unauthorized request!" });
   }
 
+  // Ověření ID Tokenu
   admin
     .auth()
     .verifyIdToken(idToken)
     .then(decodedToken => {
       req.user = decodedToken;
-      console.log(decodedToken);
+
+      // Najde dokument s daty uživatele v databázi
       return db
         .collection("users")
         .doc(req.user.uid)
         .get();
     })
     .then(doc => {
+      // Pokud existuje uživatel v databázi
       if (doc.exists) {
+        // Přiřadí uživatelské jméno do req.user.username
         req.user.username = doc.data().username;
         return next();
       } else {
-        console.error("User not found!");
         return res.status(403).json("User not found!");
       }
     })
